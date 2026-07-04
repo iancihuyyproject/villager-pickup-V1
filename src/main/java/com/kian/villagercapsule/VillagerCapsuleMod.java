0d70fbfebafa214bfab571c701781d1798685b6f
iancihuyyproject <ijawa994@gmail.com>
2026-07-04 21:38:01 +0700

    Create src/main/java/com/kian/villagercapsule/VillagerCapsuleMod.java

---
+++ src/main/java/com/kian/villagercapsule/VillagerCapsuleMod.java
@@ -0,0 +1,8 @@
+package com.kian.villagercapsule;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VillagerCapsuleMod.MODID)
+public class VillagerCapsuleMod {
+    public static final String MODID = "villagercapsule";
+}
